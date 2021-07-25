61e1bf2dd6b09c25b2f38229fdb3599972e09d34
Malay <malay.hazarika@gmail.com>
2021-07-25 22:05:52 +0530

    Idk what I am doing

---
+++ statixapp/src/App.tsx
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+ import React from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+ } from 'react-native';
+
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
+
+ import { NavigationContainer } from '@react-navigation/native';
+
+ const App = () => {
+   return (
+     <NavigationContainer>
+        <View>
+          <Text>Hello world!</Text>
+        </View>
+     </NavigationContainer>
+   );
+ };
+
+
+ export default App;
